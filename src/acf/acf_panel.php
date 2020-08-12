4364ac9d3ba6d27fa646e63490d521fd3bb3b191
Andy Pearson <andy@londonparkour.com>
2020-08-12 08:35:21 +0100

    Add AMP footer

--- src/acf/acf_panel.php
+++ src/acf/acf_panel.php
@@ -1,5 +1,4 @@
 <?php
-
 if( function_exists('acf_add_local_field_group') ):
 
     acf_add_local_field_group(array(
@@ -15,10 +14,34 @@ if( function_exists('acf_add_local_field_group') ):
                 'required' => 0,
                 'conditional_logic' => 0,
                 'wrapper' => array(
-                    'width' => '',
+                    'width' => '50',
                     'class' => '',
                     'id' => '',
                 ),
+                'hide_admin' => 0,
+                'post_type' => array(
+                    0 => 'page',
+                ),
+                'taxonomy' => '',
+                'allow_null' => 0,
+                'multiple' => 0,
+                'return_format' => 'id',
+                'ui' => 1,
+            ),
+            array(
+                'key' => 'field_5f32ad13ef8a8',
+                'label' => 'AMP Footer Page',
+                'name' => 'amp_footer_page',
+                'type' => 'post_object',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '50',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'hide_admin' => 0,
                 'post_type' => array(
                     0 => 'page',
                 ),
