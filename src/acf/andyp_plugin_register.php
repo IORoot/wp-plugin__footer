36fe763486ccd794acb054d39061cac008c3839c
Andy Pearson <andy@londonparkour.com>
2020-12-26 10:34:37 +0000

    Register with AndyP Plugins

---
+++ src/acf/andyp_plugin_register.php
@@ -0,0 +1,10 @@
+<?php
+
+add_action( 'plugins_loaded', function() {
+    do_action('register_andyp_plugin', [
+        'title'     => 'Footer Selector',
+        'icon'      => 'page-layout-footer',
+        'color'     => '#82B1FF',
+        'path'      => __FILE__,
+    ]);
+} );
